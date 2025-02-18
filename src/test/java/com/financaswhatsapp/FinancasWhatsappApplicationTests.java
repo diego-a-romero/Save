b69852a7d90daf7d6e96ded0fdb97eb68522c546
Diego Romero <diegobenario@gmail.com>
2025-02-18 20:34:30 -0300

    Configuração inicial do Spring com Maeven. Projeto ainda não contém código.

---
+++ src/test/java/com/financaswhatsapp/FinancasWhatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financaswhatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasWhatsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
